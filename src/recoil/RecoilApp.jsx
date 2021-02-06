ca3f51395f3dcaa9ccb7fb545022e93d6d8b2339
Szabolcs Hetei-Bako <sheteibako@abatondigital.com>
2021-02-06 16:39:23 +0100

    recoile debugger installed

--- src/recoil/RecoilApp.jsx
+++ src/recoil/RecoilApp.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import RecoilizeDebugger from 'recoilize';
 import { Main } from './Main';
 import { RecoilRoot, atom, selector, useRecoilState, useRecoilValue } from 'recoil';
 const RecoilApp = () => {
   return (
     <RecoilRoot>
+      <RecoilizeDebugger />
       <Main />
     </RecoilRoot>
   );
